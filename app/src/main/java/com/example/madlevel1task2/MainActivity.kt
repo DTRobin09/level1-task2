b97079ec6bd131a43a752879a3812e9d023a8e54
Robin <dtr.robin09@gmail.com>
2020-09-14 00:32:04 +0200

    Optimized code

--- app/src/main/java/com/example/madlevel1task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.madlevel1task2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.madlevel1task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,34 +18,34 @@ class MainActivity : AppCompatActivity() {
         binding.submitButton.setOnClickListener {
             checkAnswer()
         }
-
     }
 
     private fun checkAnswer() {
         var correctAnswers: Int = 0
         val maxCorrectAnswers: Int = 4
 
-        val answer1 = binding.answerOne.text.toString().toUpperCase()
-        val answer2 = binding.answerTwo.text.toString().toUpperCase()
-        val answer3 = binding.answerThree.text.toString().toUpperCase()
-        val answer4 = binding.answerFour.text.toString().toUpperCase()
+        val filledInAnswers = arrayOf<String>(
+            binding.answerOne.text.toString().toUpperCase(),
+            binding.answerTwo.text.toString().toUpperCase(),
+            binding.answerThree.text.toString().toUpperCase(),
+            binding.answerFour.text.toString().toUpperCase()
+        )
 
-        if (answer1 == getString(R.string.value_true)) {
-            correctAnswers++
-        }
-        if (answer2 == getString(R.string.value_false)) {
-            correctAnswers++
-        }
-        if (answer3 == getString(R.string.value_false)) {
-            correctAnswers++
-        }
-        if (answer4 == getString(R.string.value_false)) {
-            correctAnswers++
+        for (x in filledInAnswers.indices) {
+            if (x == 0 && filledInAnswers[x] == getString(R.string.value_true)) {
+                correctAnswers++
+            } else if (filledInAnswers[x] == getString(R.string.value_false)) {
+                correctAnswers++
+            }
         }
 
         when (correctAnswers) {
             maxCorrectAnswers -> allAnswersCorrect()
-            else -> Toast.makeText(this, getString(R.string.correct_answers, correctAnswers), Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(
+                this,
+                getString(R.string.correct_answers, correctAnswers),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
