c179a6215d4ea9cc8652e5f9a569d129832b214d
Robin <dtr.robin09@gmail.com>
2020-09-14 00:37:31 +0200

    Optimized code and finished the assignment

--- app/src/main/java/com/example/madlevel1task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.madlevel1task2.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,21 +26,22 @@ class MainActivity : AppCompatActivity() {
         val maxCorrectAnswers: Int = 4
 
         val filledInAnswers = arrayOf<String>(
-            binding.answerOne.text.toString().toUpperCase(),
-            binding.answerTwo.text.toString().toUpperCase(),
-            binding.answerThree.text.toString().toUpperCase(),
-            binding.answerFour.text.toString().toUpperCase()
+            binding.answerOne.text.toString().toUpperCase(Locale.ROOT),
+            binding.answerTwo.text.toString().toUpperCase(Locale.ROOT),
+            binding.answerThree.text.toString().toUpperCase(Locale.ROOT),
+            binding.answerFour.text.toString().toUpperCase(Locale.ROOT)
         )
 
         // checks if the filled in answers are right
         for (x in filledInAnswers.indices) {
             if (x == 0 && filledInAnswers[x] == getString(R.string.value_true)) {
                 correctAnswers++
-            } else if (filledInAnswers[x] == getString(R.string.value_false)) {
+            } else if (x != 0 && filledInAnswers[x] == getString(R.string.value_false)) {
                 correctAnswers++
             }
         }
 
+        //chooses which message to send
         when (correctAnswers) {
             maxCorrectAnswers -> allAnswersCorrect()
             else -> Toast.makeText(
